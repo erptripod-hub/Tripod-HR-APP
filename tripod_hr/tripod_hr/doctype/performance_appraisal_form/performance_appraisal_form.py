dd871bc3492a8a4741808dfd9662de2c026e0e20
Tripod <hr@tripod.com>
2026-04-02 06:47:52 +0000

    Fix: Individual objective fields (no table), no mandatory fields, fully customizable

--- tripod_hr/tripod_hr/doctype/performance_appraisal_form/performance_appraisal_form.py
+++ tripod_hr/tripod_hr/doctype/performance_appraisal_form/performance_appraisal_form.py
@@ -1,59 +1,26 @@
-# Copyright (c) 2026, Tripod and contributors
-# For license information, please see license.txt
-
 import frappe
 from frappe.model.document import Document
 
 class PerformanceAppraisalForm(Document):
-	def validate(self):
-		"""Validate the Performance Appraisal Form"""
-		self.validate_objectives()
+	def before_save(self):
 		self.calculate_overall_rating()
 	
-	def validate_objectives(self):
-		"""Ensure at least one objective exists"""
-		if not self.objectives:
-			frappe.throw("Please add at least one objective")
-	
 	def calculate_overall_rating(self):
-		"""Calculate overall rating as average of objective ratings"""
-		if not self.objectives:
-			return
+		"""Calculate overall rating from all objectives"""
+		ratings = []
 		
-		total_rating = 0
-		rated_objectives = 0
+		# Collect all objective ratings
+		for i in range(1, 7):
+			rating_field = f"objective_{i}_rating"
+			rating = self.get(rating_field)
+			if rating:
+				try:
+					ratings.append(int(rating))
+				except:
+					pass
 		
-		for obj in self.objectives:
-			if obj.rating:
-				total_rating += int(obj.rating)
-				rated_objectives += 1
-		
-		if rated_objectives > 0:
-			avg_rating = total_rating / rated_objectives
-			# Round to nearest integer
-			self.overall_rating = str(round(avg_rating))
-	
-	def on_submit(self):
-		"""Actions to perform when appraisal is submitted"""
-		self.send_notification_to_employee()
-	
-	def send_notification_to_employee(self):
-		"""Send email notification to employee when appraisal is completed"""
-		if self.employee:
-			employee_email = frappe.db.get_value("Employee", self.employee, "user_id")
-			if employee_email:
-				frappe.sendmail(
-					recipients=[employee_email],
-					subject=f"Performance Appraisal Completed - {self.review_period}",
-					message=f"""
-						<p>Dear {self.employee_name},</p>
-						<p>Your performance appraisal for <strong>{self.review_period}</strong> has been completed.</p>
-						<p><strong>Overall Rating:</strong> {self.overall_rating}/5</p>
-						<p>Please log in to the system to view your detailed appraisal.</p>
-						<p>Link: <a href="{frappe.utils.get_url()}/app/performance-appraisal-form/{self.name}">View Appraisal</a></p>
-						<br>
-						<p>Best regards,<br>HR Team</p>
-					""",
-					reference_doctype=self.doctype,
-					reference_name=self.name
-				)
+		# Calculate average
+		if ratings:
+			self.overall_rating = sum(ratings) / len(ratings)
+		else:
+			self.overall_rating = 0
--- tripod_hr/tripod_hr/doctype/performance_appraisal_objective/performance_appraisal_objective.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2026, Tripod and contributors
-# For license information, please see license.txt
-
-from frappe.model.document import Document
-
-class PerformanceAppraisalObjective(Document):
-	pass
